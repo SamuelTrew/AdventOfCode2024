ca4ce6e2c9c5189070fc8ea447cab82214244297
Samuel Trew <samuel.trew@outlook.com>
2024-12-01 15:32:03 +0000

    Formatted ocaml files

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,4 @@
 let res1 = One.part1 () in
 let res2 = One.part2 () in
-
-let () =
-  Printf.printf "%i\n" res1 in
-  Printf.printf "%i\n" res2
+let () = Printf.printf "%i\n" res1 in
+Printf.printf "%i\n" res2
--- bin/one.ml
+++ bin/one.ml
@@ -1,15 +1,16 @@
 let get_pairs (line : string) =
   let l = Str.split (Str.regexp_string "   ") line in
   match l with
-  | a :: b :: [] -> (int_of_string a, int_of_string b)
-  | _ -> (Int.max_int, Int.max_int)
+  | [ a; b ] -> int_of_string a, int_of_string b
+  | _ -> Int.max_int, Int.max_int
 ;;
 
-let rec sum_list (list: int list) =
+let rec sum_list (list : int list) =
   match list with
-  | a :: b :: rest -> Int.abs a + Int.abs b + sum_list(rest)
+  | a :: b :: rest -> Int.abs a + Int.abs b + sum_list rest
   | a :: [] -> Int.abs a
   | [] -> 0
+;;
 
 let part1 () =
   let input1 = Utils.read_file "inputs/1.txt" in
@@ -19,18 +20,28 @@ let part1 () =
   let sorted2 = List.fast_sort Int.sub (List.map snd pairs) in
   let diff = List.map2 Int.sub sorted1 sorted2 in
   sum_list diff
-
-module IntMap = Map.Make(Int)
-
-let count_frequencies lst =
-  List.fold_left (fun map x ->
-    let current_count = try IntMap.find x map with Not_found -> 0 in
-    IntMap.add x (current_count + 1) map
-  ) IntMap.empty lst
 ;;
 
-let similarity (counter: int IntMap.t) (item : int) : int =
-  item * try IntMap.find item counter with Not_found -> 0
+module IntMap = Map.Make (Int)
+
+let count_frequencies lst =
+  List.fold_left
+    (fun map x ->
+       let current_count =
+         try IntMap.find x map with
+         | Not_found -> 0
+       in
+       IntMap.add x (current_count + 1) map)
+    IntMap.empty
+    lst
+;;
+
+let similarity (counter : int IntMap.t) (item : int) : int =
+  item
+  *
+  try IntMap.find item counter with
+  | Not_found -> 0
+;;
 
 let part2 () =
   let input1 = Utils.read_file "inputs/1.txt" in
@@ -38,3 +49,4 @@ let part2 () =
   let pairs = List.map get_pairs lines in
   let counter = count_frequencies (List.map snd pairs) in
   sum_list (List.map (similarity counter) (List.map fst pairs))
+;;
